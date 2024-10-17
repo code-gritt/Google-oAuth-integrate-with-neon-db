0f4fb6d35131e741fe941ef542ad4b6f9897f690
gokul <gokulchandan24@gmail.com>
2024-10-17 19:09:56 +0530

    navbar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
-import Head from "next/head";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Reddit",
@@ -12,11 +16,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <Head>
-        <link rel="icon" type="image/x-icon" href="/reddit.png"></link>
-      </Head>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
